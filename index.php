736a47a3e43c33dd9b7907bb0674ad4c502b56b6
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-12-10 21:12:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,32 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
+ <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        h1 {
+            color: #333;
+            text-align: center;
+            background-color: #61dafb;
+            padding: 20px;
+            border-radius: 8px;
+        }
+
+        footer {
+            margin-top: 20px;
+            text-align: center;
+        }
+    </style>
       <h1>Final Project</h1>
 <?php
 include "view-footer.php";
