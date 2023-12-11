8bc78374bcfde05a3fcbf434634864fb1d58735a
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-12-11 15:08:19 -0600

    Update view-task-editform.php

--- view-task-editform.php
+++ view-task-editform.php
@@ -1,6 +1,5 @@
-
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editTaskModal<?php echo $task['task_id']; ?>">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
     </svg>
@@ -8,39 +7,38 @@
 </button>
 
 <div class="modal fade" id="editTaskModal<?php echo $task['task_id']; ?>" tabindex="-1" aria-labelledby="editTaskModalLabel<?php echo $task['task_id']; ?>" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editTaskModalLabel<?php echo $task['task_id']; ?>">Edit Task</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form method="post" action="">
-            <div class="mb-3">
-              <label for="tName <?php echo $task['task_id']; ?>" class="form-label">Task Name</label>
-              <input type="text" class="form-control" id="tName<?php echo $task['task_id']; ?>" name="tName" value="<?php echo $task['task_name']; ?>">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="editTaskModalLabel<?php echo $task['task_id']; ?>">Edit Task</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="mb-3">
-              <label for="desc <?php echo $task['task_id']; ?>" class="form-label">Description</label>
-              <input type="text" class="form-control" id="desc<?php echo $task['description']; ?>" name="desc" value="<?php echo $task['description']; ?>">
-            </div>
-            <div class="mb-3">
-              <label for="prio <?php echo $task['task_id']; ?>" class="form-label">Priority</label>
-              <input type="text" class="form-control" id="prio<?php echo $task['priority']; ?>" name="prio" value="<?php echo $task['priority']; ?>">
-            </div>
-              <div class="mb-3">
-              <label for="uid" class="form-label">User ID</label>
+            <div class="modal-body">
+                <form method="post" action="">
+                    <div class="mb-3">
+                        <label for="tName<?php echo $task['task_id']; ?>" class="form-label">Task Name</label>
+                        <input type="text" class="form-control" id="tName<?php echo $task['task_id']; ?>" name="tName" value="<?php echo $task['task_name']; ?>">
+                    </div>
+                    <div class="mb-3">
+                        <label for="desc<?php echo $task['task_id']; ?>" class="form-label">Description</label>
+                        <input type="text" class="form-control" id="desc<?php echo $task['task_id']; ?>" name="desc" value="<?php echo $task['description']; ?>">
+                    </div>
+                    <div class="mb-3">
+                        <label for="prio<?php echo $task['task_id']; ?>" class="form-label">Priority</label>
+                        <input type="text" class="form-control" id="prio<?php echo $task['task_id']; ?>" name="prio" value="<?php echo $task['priority']; ?>">
+                    </div>
+                    <div class="mb-3">
+                        <label for="uid" class="form-label">User ID</label>
 <?php
 $userList = selectUserForInput();
 include "view-user-input-list.php";
-
 ?>
+                    </div>
+                    <input type="hidden" name="tid" value="<?php echo $task['task_id']; ?>">
+                    <input type="hidden" name="actionType" value="Edit">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </form>
             </div>
-                <input type="hidden" name="tid" value="<?php echo $task['task_id']; ?>">
-                <input type="hidden" name="actionType" value="Edit">
-            <button type="submit" class="btn btn-primary">Save</button>
-        </form>
-      </div>
+        </div>
     </div>
-  </div>
-</div>  
+</div>
