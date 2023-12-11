40e57b1a71764c433e0b1f5d0b530c203caeacd3
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-12-11 13:55:36 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -6,6 +6,7 @@
     <title> <?=$pageTitle?> </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
   </head>
   <body>
     <div class="container">
