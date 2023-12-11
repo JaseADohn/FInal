770b39a8c00ac5c7ba9f45237d05a50d38cf3882
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-12-10 21:14:57 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
- <style>
+<style>
         body {
             font-family: 'Arial', sans-serif;
-            background-color: #f4f4f4;
             margin: 0;
             padding: 0;
             display: flex;
@@ -13,12 +12,13 @@ include "view-header.php";
             align-items: center;
             justify-content: center;
             height: 100vh;
+            background: linear-gradient(45deg, #ffcc00, #ffcc00 40%, #ffd699 40%, #ffd699 60%, #ff6666 60%, #ff6666);
         }
 
         h1 {
             color: #333;
             text-align: center;
-            background-color: #61dafb;
+            background-color: rgba(255, 255, 255, 0.8); 
             padding: 20px;
             border-radius: 8px;
         }
@@ -26,9 +26,10 @@ include "view-header.php";
         footer {
             margin-top: 20px;
             text-align: center;
+            color: #fff;
         }
     </style>
-      <h1>Final Project</h1>
+      <h1>Final Project: To Do Sticky Notes</h1>
 <?php
 include "view-footer.php";
 ?>
