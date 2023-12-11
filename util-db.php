0fa12a9fa30875c98b915d4539820a15ca8045c4
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-12-10 18:52:23 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('159.89.47.44', 'dohnoucr_HW3user', 'Baller120493!!', 'dohnoucr_HW3');
+    $conn = new mysqli('159.89.47.44', 'dohnoucr_HW3user', 'Baller120493!!', 'dohnoucr_Final');
     
     // Check connection
     if ($conn->connect_error) {
