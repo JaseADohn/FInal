45195fcae359928a3ecf8e8a052f291bd9d68abd
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-12-11 17:40:36 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/animejs@3.2.0"></script>
     <style>
         .navbar {
             background-color: #ffcc00;
