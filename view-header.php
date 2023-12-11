9f9d546643d06e550d3b187bcc25b18920136c9f
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-12-10 19:42:14 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -23,7 +23,7 @@
           <a class="nav-link" href="menu.php">Menu</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="">?</a>
+          <a class="nav-link" href="users.php">Users</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="">?</a>
