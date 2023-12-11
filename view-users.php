c97880a5d70c0cd907ed7978bce977fdb2502168
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-12-10 21:19:51 -0600

    Update view-users.php

--- view-users.php
+++ view-users.php
@@ -5,7 +5,8 @@
             font-family: 'Arial', sans-serif;
             margin: 0;
             padding: 0;
-            background-color: #f4f4f4;
+            background: url('background-pattern.jpg');
+            background-size: cover;
             display: flex;
             flex-direction: column;
             align-items: center;
