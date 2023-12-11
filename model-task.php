6212e0e0b09a023e99db21bfee4be503d4c49663
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-12-11 10:59:21 -0600

    Update model-task.php

--- model-task.php
+++ model-task.php
@@ -2,7 +2,7 @@
 function selectTask() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT task_id, task_name, description, priority, user_id FROM `tasks` ");
+        $stmt = $conn->prepare("SELECT task_id, task_name, description, priority, user_id FROM `task` ");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -30,7 +30,7 @@ function selectUserForInput() {
 function insertTask($tName, $uid, $desc, $prio) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `tasks` (`task_name`, `user_id`, 'description', 'priority') VALUES (?, ?)");
+        $stmt = $conn->prepare("INSERT INTO `task` (`task_name`, `user_id`, 'description', 'priority') VALUES (?, ?)");
         $stmt->bind_param("sisi", $tName, $uid, $desc, $prio);
         $success = $stmt->execute();
         $conn->close();
@@ -44,7 +44,7 @@ function insertTask($tName, $uid, $desc, $prio) {
 function updateTask($tName, $tid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("UPDATE `tasks` SET `task_name` = ?, 'description' = ?, 'priority' = ?, WHERE `task_id` = ?");
+        $stmt = $conn->prepare("UPDATE `task` SET `task_name` = ?, 'description' = ?, 'priority' = ?, WHERE `task_id` = ?");
         $stmt->bind_param("si", $tName, $tid);
         $success = $stmt->execute();
         $conn->close();
@@ -59,7 +59,7 @@ function updateTask($tName, $tid) {
 function deleteTask($tid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("DELETE FROM tasks WHERE `task`.`task_id` = ?");
+        $stmt = $conn->prepare("DELETE FROM task WHERE `task`.`task_id` = ?");
         $stmt->bind_param("i", $tid);
         $success = $stmt->execute();
         $conn->close();
