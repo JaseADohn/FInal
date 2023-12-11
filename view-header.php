86b92b38b38d706ef737e66861c6955cf74832dc
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-12-11 10:22:33 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,7 +20,7 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="menu.php">Menu</a>
+          <a class="nav-link" href="task.php">Tasks</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="users.php">Users</a>
