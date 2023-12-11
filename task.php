7907b2a349e1c0923d034cd2f249b51d715144d8
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-12-11 11:35:45 -0600

    Update task.php

--- task.php
+++ task.php
@@ -8,17 +8,17 @@ if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
     case "Add":
       if (insertTask($_POST['tName'], $_POST['uid'], $_POST['desc'], $_POST['prio'])) {
-          echo '<div class="alert alert-success" role="alert"> Product added </div>';
+          echo '<div class="alert alert-success" role="alert"> Task added </div>';
         } 
         break;
         case "Edit":
       if (updateTask($_POST['tName'], $_POST['tid'], $_POST['desc'], $_POST['prio'], $_POST['uid'])) {
-          echo '<div class="alert alert-success" role="alert"> Product Edited </div>';
+          echo '<div class="alert alert-success" role="alert"> Task Edited </div>';
         } 
         break;
     case "Delete":
       if (deleteTask($_POST['tid'])) {
-          echo '<div class="alert alert-success" role="alert"> Product deleted </div>';
+          echo '<div class="alert alert-success" role="alert"> Task deleted </div>';
         } 
         break;
     }
