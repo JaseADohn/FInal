fe754e52ff77489e2231fedc3f4a5f0452133422
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-12-11 11:13:42 -0600

    Update view-task.php

--- view-task.php
+++ view-task.php
@@ -1,5 +1,41 @@
 <div class ="row">
   <div class="col">
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            background: linear-gradient(45deg, #ffcc00, #ffcc00 40%, #ffd699 40%, #ffd699 60%, #ff6666 60%, #ff6666);
+        }
+
+        h1 {
+            color: #333;
+            text-align: center;
+            background-color: rgba(255, 255, 255, 0.8); 
+            padding: 20px;
+            border-radius: 8px;
+        }
+
+      .user-card {
+            background-color: rgba(255, 255, 255, 0.8);
+            margin: 20px;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            text-align: center;
+        }
+
+        footer {
+            margin-top: 20px;
+            text-align: center;
+            color: #fff;
+        }
+    </style>
     <h1>Tasks</h1>
   </div>
   <div class="col-auto">
