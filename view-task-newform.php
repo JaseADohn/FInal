7cee0ee1f6b3d709bf0e35adcfd7d037fa62dae9
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-12-11 10:52:45 -0600

    Create view-task-newform.php

---
+++ view-task-newform.php
@@ -0,0 +1,46 @@
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newTaskModal">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+    </svg>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="newTaskModal" tabindex="-1" aria-labelledby="newTaskModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="newTaskModalLabel">New Task</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="">
+            <div class="mb-3">
+              <label for="tName" class="form-label">Task Name</label>
+              <input type="text" class="form-control" id="tName" name="tName">
+            </div>
+          <div class="mb-3">
+              <label for="desc" class="form-label">description</label>
+              <input type="text" class="form-control" id="desc" name="desc">
+          </div>
+          <div class="mb-3">
+              <label for="prio" class="form-label">Priority</label>
+              <input type="text" class="form-control" id="prio" name="prio">
+          </div>
+              <div class="mb-3">
+              <label for="uid" class="form-label">User ID</label>
+<?php
+$userList = selectUserForInput();
+include "view-user-input-list.php";
+
+?>
+            </div>
+                <input type="hidden" name="actionType" value="Add">
+            <button type="submit" class="btn btn-primary">Save</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
